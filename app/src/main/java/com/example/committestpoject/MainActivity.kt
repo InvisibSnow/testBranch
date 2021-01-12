920c85796fd40de75c49d52ad2546c585539ffeb
Ivan <ivan.pursheha@gmail.com>
2021-01-12 21:32:07 +0200

    second commit

--- app/src/main/java/com/example/committestpoject/MainActivity.kt
+++ app/src/main/java/com/example/committestpoject/MainActivity.kt
@@ -8,6 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        hello()
 
+    }
+
+    fun hello(){
+        print("Hello world")
     }
 }
