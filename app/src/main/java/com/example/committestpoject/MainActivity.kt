15e814f2c74f113aec0fd34b823206d576b3a3e3
Ivan <ivan.pursheha@gmail.com>
2021-01-13 10:19:33 +0200

    branch2

--- app/src/main/java/com/example/committestpoject/MainActivity.kt
+++ app/src/main/java/com/example/committestpoject/MainActivity.kt
@@ -8,12 +8,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        hello()
 
+        hello()
     }
 
     fun hello(){
         print("Hello world")
+    }
+
+    fun fromBranch2(){
+        print("hello")
     }
 }
